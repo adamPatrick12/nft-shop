6c80a06f36e1d14c44a01c527f747d097ec1dea3
adamPatrick12 <adam_77paper@hotmail.com>
2021-12-29 22:54:26 -0500

    added more css to main screen

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Logo from "./images/logo.png"
+import Button from '@mui/material/Button';
 
 
 const theme = createTheme({
@@ -31,6 +32,21 @@ function Home() {
             <a  className='menuBtn' href="">Home</a>
             <a  className='menuBtn' href="">Shop</a>
             <a  className='menuBtn' href="">What are NFT's</a>
+          </div>
+          <div className='pageInfo'>
+              <div className='infoBox1'>
+                  <h2>
+                      ClosedSea - The online marketplace to Discover, collect, and sell NFTs
+                  </h2>
+              </div>
+              <div className='infoBox2'>
+                 <h3>
+                     ClosedSea is the world's first and largest fake NFT marketplace! Why buy NFTs with your hard earned cash when, you can just screenshot them!  
+                 </h3>
+                
+                <button className='shopBtn' >Shop NFTs</button>
+
+              </div>
           </div>
       </div>
 
