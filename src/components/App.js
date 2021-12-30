ec7a99320f072b8c84dd6167f39346f55bdf489b
adamPatrick12 <adam_77paper@hotmail.com>
2021-12-30 18:25:38 -0500

    Finished menu screen

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Logo from "./images/logo.png"
 import Button from '@mui/material/Button';
-
+import {motion} from "framer-motion"
+import HomeNFT from "./images/stolenNFT.png"
+import Eth from "./images/eth2.svg"
+import clock from "./images/clock.svg"
+import Logo from "./images/logo.gif"
 
 const theme = createTheme({
   typography: {
@@ -20,15 +23,26 @@ function Home() {
   
   return (
     <div className="Home-BG">
-      <div className='circle1'>
+      <motion.div 
+      className='circle1'
+        animate={{ y: [0, 35, 0] }}
+        transition={{ repeat: Infinity, duration: 10 }}
+      >
 
-      </div>
-      <div className='circle2'>
+      </motion.div>
+      <motion.div 
+      className='circle2'
+      animate={{ y: [0, 35, 0] }}
+      transition={{ repeat: Infinity, duration: 10 }}
+      >
 
-      </div>
+      </motion.div>
       <div className='glass'>
           <div className='header'>    
-           <img className='logo' src={Logo} alt="" />
+           <div className='logoDiv'>
+             <img className='logo' src={Logo} alt="" />
+             <h1>ClosedSea</h1>
+           </div>
             <a  className='menuBtn' href="">Home</a>
             <a  className='menuBtn' href="">Shop</a>
             <a  className='menuBtn' href="">What are NFT's</a>
@@ -44,10 +58,42 @@ function Home() {
                      ClosedSea is the world's first and largest fake NFT marketplace! Why buy NFTs with your hard earned cash when, you can just screenshot them!  
                  </h3>
                 
-                <button className='shopBtn' >Shop NFTs</button>
+                <motion.button 
+                className='shopBtn' 
+                whileHover={{ scale: 1.1 }}
+                >Shop NFTs</motion.button>
 
               </div>
+              <div className = "nftContainer" >
+                  <motion.div 
+                  className='icon'
+                  animate={{ y: [0, 35, 0] }}
+                  transition={{ repeat: Infinity, duration: 10 }}
+                  >
+                    <div>
+                        <img className='nftImg' src={HomeNFT} alt="" />
+                    </div>
+                    <div className='nftInfo'>
+                      <h4>CloneX #17580</h4>
+                    </div>
+                    <div className='nftInfo2'>
+                    <p>Virtual fashion collective by RTFKT Studios. A clear homage to the CryptoPunks series </p>
+                    </div>
+                    <div className='nftEth'>
+                        <div className='nftEth2'>
+                        <img className= "nftEth-Img" src={Eth} alt="" />
+                        <span>44 Eth </span>
+                        </div> 
+                        <div className='nftEth2'>
+                        <img className= "nftEth-Img" src={clock} alt="" />
+                        <span>3 Days Left </span>
+                        </div> 
+                    </div>
+                  </motion.div>
+              </div>
           </div>
+          
+          
       </div>
 
     </div>
