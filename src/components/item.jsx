87d829bd9d510f87099d93cc3ff27c7290eddf93
adamPatrick12 <adam_77paper@hotmail.com>
2022-01-01 22:34:16 -0500

    implemented opensea API

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion"
 import Eth from "./images/eth2.svg"
 import clock from "./images/clock.svg"
@@ -7,39 +7,37 @@ import '../store.css';
 import ShoppingCart from "./images/shopping.png"
 import Ape from "./images/BAYC.png"
 
+
 export default function Item() {
   
-  const element1 =  <motion.div
-  className='icon2'
-  animate={{ y: [0, 10, 0] }}
-  transition={{ repeat: Infinity, duration: 0 }}
->
-  <div>
-    <img className='nftImg' src={HomeNFT} alt="" />
-  </div>
-  <div className='nftInfo'>
-    <h4>CloneX #17580</h4>
-  </div>
-  <div className='nftInfo2'>
-    <p>Virtual fashion collective by RTFKT Studios. A clear homage to the CryptoPunks series </p>
-  </div>
-  <div className='nftEth'>
-    <div className='nftEth2'>
-      <img className="nftEth-Img" src={Eth} alt="" />
-      <span>44 Eth </span>
-    </div>
-  <motion.button 
-  whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.9 }}  
-   className="buyBtn"> BUY 
-   <img className="shoppingCart" src={ShoppingCart} alt="" /> 
-    </motion.button>
-    <div className='nftEth2'>
-      <img className="nftEth-Img" src={clock} alt="" />
-      <span>3 Days</span>
-    </div>
-  </div>
-</motion.div>
+  useEffect(() => {
+    getNFT()
+  }, [])
+  
+  const [png, setPng] = useState([])  
+  
+  async function getNFT() {
+    
+
+      
+    try {
+        const response = await fetch(
+          `https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=25&collection=doodles-official`,
+          { mode: 'cors' },
+        );
+        const nftData = await response.json();
+        const nftArr = nftData.assets
+        console.log(nftArr)
+        setPng(nftArr)
+      } catch {
+        console.log("ERROR")
+        }
+        
+    }
+
+
+
+
 
 const element2 =  <motion.div
   className='icon2'
@@ -76,9 +74,42 @@ const element2 =  <motion.div
   return (
     
     <div>
-     {element1} 
-     {element2} 
+    
+    {png.map(images => (
+    <motion.div
+    className='icon2'
+    animate={{ y: [0, 10, 0] }}
+    transition={{ repeat: Infinity, duration: 0 }}
+  >
+    <div>
+      <img className='nftImg' src={images.image_url} alt="" />
+    </div>
+    <div className='nftInfo'>
+      <h4>{images.name}</h4>
+    </div>
+    <div className='nftInfo2'>
+      <p>Virtual fashion collective by RTFKT Studios. A clear homage to the CryptoPunks series </p>
+    </div>
+    <div className='nftEth'>
+      <div className='nftEth2'>
+        <img className="nftEth-Img" src={Eth} alt="" />
+        <span>44 Eth </span>
+      </div>
+    <motion.button 
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}  
+     className="buyBtn"> BUY 
+     <img className="shoppingCart" src={ShoppingCart} alt="" /> 
+      </motion.button>
+      <div className='nftEth2'>
+        <img className="nftEth-Img" src={clock} alt="" />
+        <span>3 Days</span>
+      </div>
+    </div>
+  </motion.div>
      
+    ))}
+
     </div>
   
    
